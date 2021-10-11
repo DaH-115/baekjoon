d65195ce00bd2cbe5c3014e307ad48215f791dfe
da-yooom <kdh941110@gmail.com>
2021-10-11 23:32:02 +0900

    ✨ 입출력과 사칙연산 완료

---
+++ 입출력과 사칙연산/b2557-10718-10171-10172/app.js
@@ -0,0 +1,15 @@
+console.log('Hello World!');
+
+console.log('강한친구 대한육군');
+console.log('강한친구 대한육군');
+
+console.log('\\    /\\');
+console.log(" )  ( ')");
+console.log('(  /  ) ');
+console.log(' \\(__)| ');
+
+console.log('|\\_/|');
+console.log('|q p|   /}');
+console.log('( 0 )"""\\');
+console.log('|"^"`    |');
+console.log('||_/=\\\\__|');
