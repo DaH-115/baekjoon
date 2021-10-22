8c83335fc213bd19e2bab1beaa4725efd9bfe9c7
da-yooom <kdh941110@gmail.com>
2021-10-23 01:33:18 +0900

    ✨ 문자열 완료

--- 문자열/b1316/app.js
+++ 문자열/b1316/app.js
@@ -1,4 +1,30 @@
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.text';
 let input = require('fs').readFileSync(filePath).toString().trim().split('\n');
 
-console.log(input);
+const caseNum = Number(input[0]);
+const caseArr = input;
+let count = 0;
+
+solution(caseNum, caseArr);
+
+function solution(caseNum, caseArr) {
+  for (let i = 1; i <= caseNum; i++) {
+    const word = caseArr[i];
+    const strArr = [];
+    let isGroupWord = true;
+
+    for (let j = 0; j < word.length; j++) {
+      if (strArr.indexOf(word[j]) === -1) {
+        strArr.push(word[j]);
+      } else if (strArr.indexOf(word[j]) !== strArr.length - 1) {
+        isGroupWord = false;
+      }
+    }
+
+    if (isGroupWord) {
+      count += 1;
+    }
+  }
+
+  console.log(count);
+}
